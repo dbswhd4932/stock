7d4574e7d910d2dcd17091a4d3daecd3b502df88
yoonjong <dbswhd4932@gmail.com>
2023-03-01 22:38:08 +0900

    Named Lock 활용 - 보통 사용 X

--- src/main/java/com/example/stockman/facade/OptimisticLockStockFacade.java
+++ src/main/java/com/example/stockman/facade/OptimisticLockStockFacade.java
@@ -1,9 +1,10 @@
 package com.example.stockman.facade;
 
 import com.example.stockman.service.OptimisticLockStockService;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class OptimisticLockStockFacade {
 
     private OptimisticLockStockService optimisticLockStockService;
--- src/main/java/com/example/stockman/service/StockService.java
+++ src/main/java/com/example/stockman/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stockman.service;
 import com.example.stockman.domain.Stock;
 import com.example.stockman.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,7 +15,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     // 1개의 스레드만 접근 가능하게 함
     public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
